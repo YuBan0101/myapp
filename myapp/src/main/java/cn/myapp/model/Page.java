710b2869f27aad162f75d1acfbddee9493108515
master <ssxxvvc@qq.com>
2018-07-03 10:35:01 +0800

    建立Page model

---
+++ myapp/src/main/java/cn/myapp/model/Page.java
@@ -0,0 +1,43 @@
+package cn.myapp.model;
+
+public class Page {
+	
+	private Integer currentPage;
+	private Integer prePage;
+	private Integer nextPage;
+	private Integer pageCount;
+	private Integer showCount;
+	
+	public Integer getCurrentPage() {
+		return currentPage;
+	}
+	public void setCurrentPage(Integer currentPage) {
+		this.currentPage = currentPage;
+	}
+	public Integer getPrePage() {
+		return prePage;
+	}
+	public void setPrePage(Integer prePage) {
+		this.prePage = prePage;
+	}
+	public Integer getNextPage() {
+		return nextPage;
+	}
+	public void setNextPage(Integer nextPage) {
+		this.nextPage = nextPage;
+	}
+	public Integer getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(Integer pageCount) {
+		this.pageCount = pageCount;
+	}
+	public Integer getShowCount() {
+		return showCount;
+	}
+	public void setShowCount(Integer showCount) {
+		this.showCount = showCount;
+	}
+	
+
+}
