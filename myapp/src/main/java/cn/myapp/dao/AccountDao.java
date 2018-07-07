3d0ee8798e1f69709aedf32eb27423235471d533
master <ssxxvvc@qq.com>
2018-07-07 17:48:01 +0800

    生成account 表模型  做一个记账小本本

---
+++ myapp/src/main/java/cn/myapp/dao/AccountDao.java
@@ -0,0 +1,17 @@
+package cn.myapp.dao;
+
+import cn.myapp.model.Account;
+
+public interface AccountDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Account record);
+
+    int insertSelective(Account record);
+
+    Account selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Account record);
+
+    int updateByPrimaryKey(Account record);
+}
---
+++ myapp/src/main/java/cn/myapp/model/Account.java
@@ -0,0 +1,75 @@
+package cn.myapp.model;
+
+import java.util.Date;
+
+public class Account {
+    private Integer id;
+
+    private String content;
+
+    private Double money;
+
+    private Date date;
+
+    private Byte status;
+
+    private String phone;
+
+    private String remark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}
